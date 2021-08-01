1a9e6c41259acf45fef77d1549d4dd8cd5ba751d
Arinze Anozie <arinze.anozie@aetainvestments.com>
2021-08-01 18:08:45 -0400

    Add extension method to find tables in assembly (based on AliasAttribute)

---
+++ src/Aeta.Extensions.Pulumi.PocoDynamo/ReflectionExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using ServiceStack.DataAnnotations;
+
+namespace Aeta.Extensions.Pulumi.PocoDynamo
+{
+    public static class ReflectionExtensions
+    {
+        public static IEnumerable<TableGroup> FindTables(this Assembly assembly)
+        {
+            return assembly.GetTypes()
+                .Select(type => (type,
+                    attribute: (AliasAttribute) Attribute.GetCustomAttribute(type, typeof(AliasAttribute))))
+                .Where(metadata => metadata.attribute is not null)
+                .GroupBy(metadata => metadata.attribute.Name)
+                .Select(group => new TableGroup(group.Key, group.Select(g => g.type)));
+        }
+    }
+}
