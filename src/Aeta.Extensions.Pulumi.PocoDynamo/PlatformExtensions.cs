c7f640d5444b01b56e20995b83c1bbd60eabcbbf
Arinze Anozie <arinze.anozie@aetainvestments.com>
2021-08-01 19:38:26 -0400

    Add assembly scanning extension for AliasAttributed tables

--- src/Aeta.Extensions.Pulumi.PocoDynamo/PlatformExtensions.cs
+++ src/Aeta.Extensions.Pulumi.PocoDynamo/PlatformExtensions.cs
@@ -6,16 +6,16 @@ using ServiceStack.DataAnnotations;
 
 namespace Aeta.Extensions.Pulumi.PocoDynamo
 {
-    public static class ReflectionExtensions
+    public static class PlatformExtensions
     {
-        public static IEnumerable<TableGroup> FindTables(this Assembly assembly)
+        public static IEnumerable<TableBuilder> FindPocoDynamoTableDefinitions(this Assembly assembly)
         {
             return assembly.GetTypes()
                 .Select(type => (type,
                     attribute: (AliasAttribute) Attribute.GetCustomAttribute(type, typeof(AliasAttribute))))
                 .Where(metadata => metadata.attribute is not null)
                 .GroupBy(metadata => metadata.attribute.Name)
-                .Select(group => new TableGroup(group.Key, group.Select(g => g.type)));
+                .Select(group => new TableBuilder(group.Key, group.Select(g => g.type)));
         }
     }
 }
---
+++ tests/Aeta.Extensions.Pulumi.PocoDynamo.Tests/PlatformExtensionsTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using FluentAssertions;
+using ServiceStack.DataAnnotations;
+using Xunit;
+
+namespace Aeta.Extensions.Pulumi.PocoDynamo.Tests
+{
+    public class PlatformExtensionsTests
+    {
+        [Fact]
+        public void FindTables_Should_Find_Table_In_Assembly_Decorated_With_AliasAttribute()
+        {
+            var assembly = typeof(TableToFind).Assembly;
+            var builders = assembly.FindPocoDynamoTableDefinitions().ToList();
+            
+            builders.Should().NotBeEmpty();
+            builders.Should().Contain(builder => builder.TableName == "TableToFind");
+        }
+        
+        [Fact]
+        public void FindTables_Should_Find_Polymorphic_Tables_In_Assembly_Decorated_With_AliasAttribute()
+        {
+            var assembly = typeof(TableToFind).Assembly;
+            var builders = assembly.FindPocoDynamoTableDefinitions().ToList();
+            var builder = builders.Find(b => b.TableName == "TableToFind");
+
+            builder.Should().NotBeNull();
+            builder!.Metadatas.Select(m => m.Type)
+                .Should().BeEquivalentTo(new object[]
+                {
+                    typeof(TableToFind),
+                    typeof(TableToFindKindOne),
+                    typeof(TableToFindKindTwo)
+                });
+        }
+    }
+
+    [Alias("TableToFind")]
+    public class TableToFind
+    {
+        [HashKey] public string HashKey { get; set; }
+    }
+    
+    [Alias("TableToFind")]
+    public class TableToFindKindOne : TableToFind {}
+    
+    
+    [Alias("TableToFind")]
+    public class TableToFindKindTwo : TableToFind {}
+}
